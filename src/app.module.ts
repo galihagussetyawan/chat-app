239afe14756220c4ed585536579499a291ed6ba9
galihagussetyawan <galihdevelopment@gmail.com>
2023-10-14 11:50:09 +0700

    feat: add configuration mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV && '.env.dev',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
